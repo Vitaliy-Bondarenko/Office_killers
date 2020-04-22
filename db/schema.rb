40523bccd6ae317d9bc4a239eaf56d5ce2721896
Dmitro Shevchenko <sheva@active-bridge.com>
2020-04-22 17:43:28 +0300

    Change target_ids column type

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_04_01_214022) do
+ActiveRecord::Schema.define(version: 2020_04_22_124057) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -31,9 +31,9 @@ ActiveRecord::Schema.define(version: 2020_04_01_214022) do
     t.integer "user_id"
     t.integer "target_id"
     t.integer "status"
-    t.integer "target_ids"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.integer "target_ids", default: [], array: true
   end
 
   create_table "users", force: :cascade do |t|
