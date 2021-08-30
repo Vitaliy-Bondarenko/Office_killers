32232944909b01d10ad667086ee12d6f80c01ece
jawny-utah <sheva@active-bridge.com>
2021-08-30 20:52:16 +0300

    Fix/Css styles on login page

--- app/javascript/bundles/HelloWorld/components/LoginPage.jsx
+++ app/javascript/bundles/HelloWorld/components/LoginPage.jsx
@@ -2,18 +2,15 @@ import React from 'react';
 
 const LoginPage = () => {
   return (
-    <div className='mm-list'>
+    <div className='mm-list' style={{top: '44%'}}>
       <h1
           className='login-font'
-          style={{marginTop: '-25%', marginBottom: '20px'}}> KILLER </h1>
+          style={{marginBottom: '20px'}}> KILLER </h1>
       <a
-          className='button-small-padding'
-          href='/login'
-          style={{marginTop: '100px',
-                  textDecoration: 'none',
-                  whiteSpace: 'nowrap'}}>
+          className='login-btn'
+          href='/login'>
         <img id='google-logo-m' src='http://tiny.cc/gc2gnz' style={{marginRight: '6px'}} />
-        SIGN IN WITH GOOGLE
+        <p>SIGN IN WITH GOOGLE</p>
       </a>
     </div>
   );
