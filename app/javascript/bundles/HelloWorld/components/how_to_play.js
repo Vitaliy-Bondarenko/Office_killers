1ecadd0b674887355deb23083b72264e39513166
jawny-utah <sheva@active-bridge.com>
2021-12-04 12:24:28 +0200

    Feature/Add how to play page

---
+++ app/javascript/bundles/HelloWorld/components/how_to_play.js
@@ -0,0 +1,56 @@
+const text = [
+  { legend: 'GAME RULE',
+    body: `Office Killer is a real life game for 3 up to as many players as you want, all about attentiveness
+           and cleverness. Main goal of every player in this game is to eliminate their target and those,
+           who get most kill count, will win.`,
+    id: 1
+  },
+  { legend: 'GAME CREATION',
+    body: `To create game go on appropriate page and click Create game button at the bottom. You can invite players in your game
+           in two ways: by special game code
+           <img align='center' src='/game_code_to_connect.png' />
+           <br />
+           Or by game QR code.`,
+    id: 2
+  },
+  {
+    legend: 'GAME CREATION',
+    body: `Also, you can set schedule for your game by setting date and time.
+           <img align='center' src='/schedule_game.png' />
+           <br />
+           After time comes, game will automatically started if there are more than 3 players connected.`,
+    id: 3
+  },
+  {
+    legend: 'JOINING TO GAME',
+    body: `To connect to existing game go to Join Game page and scan QR code from game or enter its code.`,
+    id: 4
+  },
+  {
+    legend: 'GAME PROCESS',
+    body: `After game has started, every player will get random target, that they need to eliminate. Elimination can be
+           happened by putting your arm on your target shoulder and by saying 'You're dead' to him, while there
+           are no other person in room present.`,
+    id: 5
+  },
+  {
+    legend: 'PLAYER KILLING',
+    body: `After you eliminate your taget, he needs to confirm his death by pressing button 'I was killed' on Game page.
+           After you confirm your kill on your device, you will get your victim target. Game will be finished automatically
+           after there only 2 alive player remain. Out of those, who did more kills, will win the game.`,
+    id: 6
+  },
+  {
+    legend: 'GAME ENDING',
+    body: `You can see game statistics on Game Statistics page after game has ended.`,
+    id: 7
+  },
+  {
+    legend: 'SETTINGS',
+    body: `You can change avatar, personal information and configure notification on Settings Page.
+           Be carefull, avatar image file size can't be exceed 3 mb.`,
+    id: 8
+  }
+]
+
+export default text;
